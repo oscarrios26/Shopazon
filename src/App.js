3467a0219679673c2687a8f8d5da49833c0c73c2
Mayra and Oscar <mayraandoscar@mayras-mbp.attlocal.net>
2021-12-03 22:02:02 -0600

    finish nav comp

--- src/App.js
+++ src/App.js
@@ -2,16 +2,16 @@ import {BASE_URL, config} from './services'
 import { Link, Routes, Route } from 'react-router-dom'
 import {useEffect, useState} from 'react'
 import axios from 'axios'
-import Details from './components/Details/Details'
-
-
+import Nav from './components/Nav/Nav'
+import Detail from './components/Details/Details'
+import Homepage from './components/Homepage/Homepage'
 
 
 function App() {
-
 const [games, setGames] = useState([])
 
-  useEffect(() => {
+  
+useEffect(() => {
     const getGames = async () => {
       const response = await axios.get(BASE_URL, config)
       setGames(response.data.records)
@@ -22,22 +22,17 @@ const [games, setGames] = useState([])
   
   
   
+  
+  
   return (
     <div> 
-      <Link to='/'>Home</Link>
+      <Nav/>
 
     <Routes>
       
-      <Route path='/' element={  
-      <div>
-      {games.map(vg => (
-        
-        <Link to={`/details/${vg.id}`}><img style={{width: "300px"  }} src={vg.fields.image}/></Link>
-        
-      ))}
-      </div>}/>
+      <Route path='/' element={<Homepage games={games}/>}/>
 
-      <Route path={'/details/:id'} element={<Details games={games}/>}/>
+      <Route path={'/details/:id'} element={<Detail games={games}/>}/>
       </Routes>
    
 
--- src/components/Homepage/Homepage.jsx
+++ src/components/Homepage/Homepage.jsx
@@ -1,1 +1,15 @@
+import { Link } from "react-router-dom"
 
+
+
+export default function Homepage(props) {
+    return (
+        <div>
+            {props.games.map(vg => (
+        
+        <Link to={`/details/${vg.id}`}>{vg.fields.title}<img style={{width: "300px"  }} src={vg.fields.image}/></Link>
+        
+      ))}
+        </div>
+    )
+}
